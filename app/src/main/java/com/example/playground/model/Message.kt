15e312b931f83cc6cde7e61ca90b36ec7f98f52a
Yu-Xin Lin <gx052356779@gmail.com>
2025-07-11 13:59:33 +1200

    upload1
    dd

--- app/src/main/java/com/example/playground/model/Message.kt
+++
@@ -1,9 +0,0 @@
-package com.example.playground.model
-
-data class Message(
-    val id: String = System.currentTimeMillis().toString(),
-    val content: String,
-    val isUser: Boolean,
-    val imageUrl: String? = null,
-    val isLoading: Boolean = false
-) 
